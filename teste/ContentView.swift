6a6e0e2427293c645fefa6f284dc48b278725d17
Ítalo Monte <italopessoal@outlook.com>
2024-02-29 12:42:59 -0800

    Finished Assigment 1

--- teste/ContentView.swift
+++ teste/ContentView.swift
@@ -8,7 +8,9 @@ struct ContentView: View {
     var body: some View {
         
         VStack{
-            cards
+            ScrollView (.vertical){
+                cards
+            }
             Spacer()
             cardsCountAdjusters
         }
@@ -21,6 +23,7 @@ struct ContentView: View {
         LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
             ForEach(0..<cardsCount, id: \.self){ index in
                 CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
             }
         }
         .foregroundColor(Color.red)
