15fb61b0e6be0f8704063f7b2942491fb79228a3
Ítalo Monte <italopessoal@outlook.com>
2024-04-23 09:27:59 -0700

    initial

---
+++ cs193p/cs193pApp.swift
@@ -0,0 +1,17 @@
+//
+//  cs193pApp.swift
+//  cs193p
+//
+//  Created by Italo Guilherme Monte on 23/04/24.
+//
+
+import SwiftUI
+
+@main
+struct cs193pApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
